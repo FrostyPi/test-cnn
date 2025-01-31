ceb5c0acca7ce5dc2809ba9165d4b46a7f72128f
hm <henry.m@tcc-technology.com>
2025-01-31 13:30:32 +0700

    filled in cnn

--- mycnn.py
+++ mycnn.py
@@ -18,7 +18,27 @@ testloader = DataLoader(testset, batch_size=64, shuffle=True)
 class MyCNN(nn.Module):
     def __init__(self):
         super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(32 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+
     def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = x.flatten(start_dim=1)
+
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
         return x
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -50,8 +70,10 @@ total = 0
 with torch.no_grad():
      for images, labels in testloader:
         images, labels = images.to(device), labels.to(device)
+
         outputs = model(images)
         _, predicted = torch.max(outputs, 1)
+
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
